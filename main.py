d237f0d95f901a673fe2e9c43002f910f3f39e87
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-04-27 13:50:32 +0300

    Скачивание и добавление набор библиотек GUI

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import json
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout,
+                             QHBoxLayout, QPushButton, QLabel, QLineEdit,
+                             QMessageBox, QScrollArea, QCheckBox)
+from PyQt5.QtCore import Qt, QPropertyAnimation, QEasingCurve, QPoint
+from PyQt5.QtGui import QIcon, QFont, QColor, QPalette, QLinearGradient, QBrush
+
